67d0de32292b21854d7ec548bfcc189d8ea7fd94
dpienczykowski <dpienczykowski@gmail.com>
2017-01-11 10:24:27 +0100

    fix cardComponent

--- src/components/cardComponent/cardComponent.js
+++ src/components/cardComponent/cardComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Profile from '../stateless_components/statelessComponents';
 
 class CardComponent extends Component {
--- src/components/cardList/cardList.js
+++ src/components/cardList/cardList.js
@@ -1,42 +1,44 @@
 import React, {Component} from 'react';
+import {debounce} from "lodash";
+
 import CardComponent from '../cardComponent/cardComponent';
 
 class CardList extends Component {
   constructor(props) {
     super(props);
-    this.cards = [];
-  }
 
-  render() {
-    let newArray = [{
-      id: 1,
-      name: 'asd',
-      description: 'Lorem ipusasdas da da sd a',
-    }, {
-      id: 2,
-      name: 'sasd',
-    }, {
-      id: 3,
-      name: '3asd',
-      description: 'asd asd as das dsd as da sd',
-    }];
-
-    let newArray2 = { id: 4, name: 'dupa' };
-    let cards = this.cards;
-    cards = [...newArray];
-
-    let pushSecondArray = () => {
-      console.log(cards);
-      cards.push(newArray2);
-      console.log(cards);
+    this.state = {
+      cards: [
+        {
+          id: 1,
+          name: 'asd',
+          description: 'Lorem ipusasdas da da sd a',
+        }, {
+          id: 2,
+          name: 'sasd',
+        }, {
+          id: 3,
+          name: '3asd',
+          description: 'asd asd as das dsd as da sd',
+        },
+      ]
     };
 
-    setTimeout(() => pushSecondArray(), 500);
+    this.pushSendArray();
+  }
 
+  pushSendArray() {
+    console.log("=== calling push send array");
+    debounce(() => {
+      const cards = [{id: (new Date()).getTime(), name: 'test'}, ...this.state.cards];
+      this.setState({cards});
+    }, 3000)();
+  };
+
+  render() {
     return (
       <div>
-        { this.cards.map(item => <CardComponent key={item.id} item={item}/>)
-        }
+        {this.state.cards.map(item => <CardComponent key={item.id} item={item}/>)}
       </div>
     )
   }
