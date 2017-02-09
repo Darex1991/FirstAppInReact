5d627f8b8a7bbf7e527a215f1123e07782f776ee
dpienczykowski <dpienczykowski@gmail.com>
2017-02-09 23:39:15 +0100

    phonenumbercomponent snapshot

--- src/app/views/simonTask/__tests__/PhoneNumbersComponent-test.js
+++ src/app/views/simonTask/__tests__/PhoneNumbersComponent-test.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React            from 'react';
+import ReactTestUtils   from 'react-addons-test-utils'
+
+import chai             from 'chai';
+import chaiJestSnapshot from "chai-jest-snapshot";
+
 import PhoneNumbersComponent from '../PhoneNumbersComponent';
+
+chai.use(chaiJestSnapshot);
+
 test('Should render PhoneNumbersComponent correctly', () => {
-    const tree = renderer.create(
-        <PhoneNumbersComponent /> 
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+    const props = {
+        dataBase: {
+            phones: [
+                {id: 1, name: 'asd', key: 1, value: 'val 1', checked: true, highPriority: true},
+                {id: 2, name: 'asdasdasd', key: 2, value: 'val 122222', checked: false, highPriority: false},
+                {id: 3, name: 'asdasdasd', key: 3, value: 'val asdasd', checked: false, highPriority: true},
+                {id: 4, name: '222', key: 4, value: 'val213123 1', checked: true, highPriority: false}]
+        },
+        updateDataBase: (newDataBase) => {
+            this.dataBase = newDataBase
+        }
+    };
+
+    const shallowRenderer = ReactTestUtils.createRenderer();
+    const result = shallowRenderer.render(<PhoneNumbersComponent {...props} />);
+    expect(result).toMatchSnapshot();
 });
-    
