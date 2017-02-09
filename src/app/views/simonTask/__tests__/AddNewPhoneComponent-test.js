9b1eea949f6c50fb02991870aaf9249d342da1be
dpienczykowski <dpienczykowski@gmail.com>
2017-02-09 23:46:20 +0100

    setup all tests

--- src/app/views/simonTask/__tests__/AddNewPhoneComponent-test.js
+++ src/app/views/simonTask/__tests__/AddNewPhoneComponent-test.js
@@ -2,9 +2,20 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import AddNewPhoneComponent from '../AddNewPhoneComponent';
 test('Should render AddNewPhoneComponent correctly', () => {
+    let props = {
+        dataBase: {
+            phones: [
+                {id: 1, name: 'asd', key: 1, value: 'val 1', checked: true, highPriority: true},
+                {id: 2, name: 'asdasdasd', key: 2, value: 'val 122222', checked: false, highPriority: false},
+                {id: 3, name: 'asdasdasd', key: 3, value: 'val asdasd', checked: false, highPriority: true},
+                {id: 4, name: '222', key: 4, value: 'val213123 1', checked: true, highPriority: false}]
+        },
+        updateDataBase: (newDataBase) => {
+            this.dataBase = newDataBase
+        }
+    };
     const tree = renderer.create(
-        <AddNewPhoneComponent /> 
+        <AddNewPhoneComponent {...props} />
     ).toJSON();
     expect(tree).toMatchSnapshot();
 });
-    
