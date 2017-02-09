aad0249173edcb923bb23e61477e22f4cea38f11
dpienczykowski <dpienczykowski@gmail.com>
2017-02-09 23:37:40 +0100

    input withoutvalue snapshot

--- src/app/views/simonTask/__tests__/InputWithoutValue-test.js
+++ src/app/views/simonTask/__tests__/InputWithoutValue-test.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React            from 'react';
+import ReactTestUtils   from 'react-addons-test-utils'
+
+import chai             from 'chai';
+import chaiJestSnapshot from "chai-jest-snapshot";
+
 import InputWithoutValue from '../InputWithoutValue';
+
+chai.use(chaiJestSnapshot);
+
 test('Should render InputWithoutValue correctly', () => {
-    const tree = renderer.create(
-        <InputWithoutValue /> 
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+    const shallowRenderer = ReactTestUtils.createRenderer();
+    const result = shallowRenderer.render( <InputWithoutValue /> );
+    expect(result).toMatchSnapshot();
 });
-    
